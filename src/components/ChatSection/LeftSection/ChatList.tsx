762836340e3577833ba4406b9471e226c675a107
abhi <shuklaabhinav824@gmail.com>
2025-05-10 15:23:57 +0530

    internal ui library created

---
+++ src/components/ChatSection/LeftSection/ChatList.tsx
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+import { useState } from 'react';
+import Status from './Status';
+import SearchChat from './SearchChat';
+import List from './List';
+
+const client = new PrismaClient();
+
+const ChatList = () => {
+    // const [chats, setChats] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    return (
+        <div className='flex flex-col items-center justify-between h-full w-2/5 bg-neutral-800 m-0.5 rounded-sm'>
+            <Status/>
+            <SearchChat/>
+            <List/>
+        </div>
+    )
+}
+
+export default ChatList
---
+++ src/components/ChatSection/LeftSection/SearchChat.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchChat = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchChat
